163eb9bd855e0f05f7bedb7c99a413ecb681fac2
Jakub Rodzik <jakub.r.rodzik@gmail.com>
2021-12-01 08:38:30 +0100

    day1p2

--- day1/day1.rb
+++ day1/day1.rb
@@ -12,3 +12,18 @@ end
 
 input = File.read('day1/input.txt').split("\n").map(&:to_i)
 p day1p1(input)
+
+def day1p2(input)
+  counter = 0
+  previos_sum = -Float::INFINITY
+
+  (input.length - 3).times do |i|
+    current_sum = input[i] + input[i + 1] + input[i + 2]
+    counter += 1 if current_sum > previos_sum
+    previos_sum = current_sum
+  end
+
+  counter
+end
+
+p day1p2(input)
--- day1/day1_spec.rb
+++ day1/day1_spec.rb
@@ -19,4 +19,21 @@ RSpec.describe 'Day 1' do
 
     expect(day1p1(input)).to eq(7)
   end
+
+  it 'counts the number of times the sum of measurements in this sliding window increases' do
+    input = [
+      199, #  A
+      200, #  A B
+      208, #  A B C
+      210, #    B C D
+      200, #  E   C D
+      207, #  E F   D
+      240, #  E F G
+      269, #    F G H
+      260, #      G H
+      263  #        H
+    ]
+
+    expect(day1p2(input)).to eq(5)
+  end
 end
