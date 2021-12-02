429a98ae16aa933eefb1361ea240b7df05817ee6
Jakub Rodzik <jakub.r.rodzik@gmail.com>
2021-12-02 08:39:58 +0100

    day2p2

--- day2/day2.rb
+++ day2/day2.rb
@@ -17,3 +17,25 @@ end
 
 commands = File.read('day2/input.txt').split("\n")
 p day2p1(commands)
+
+def day2p2(commands)
+  horizontal_position = 0
+  depth = 0
+  aim = 0
+
+  commands.each do |c|
+    order, value = c.split(' ')
+
+    case order
+    when 'up' then aim -= value.to_i
+    when 'down' then aim += value.to_i
+    when 'forward'
+      horizontal_position += value.to_i
+      depth += aim * value.to_i
+    end
+  end
+
+  horizontal_position * depth
+end
+
+p day2p2(commands)
--- day2/day2_spec.rb
+++ day2/day2_spec.rb
@@ -15,4 +15,17 @@ RSpec.describe 'Day 2' do
 
     expect(day2p1(commands)).to eq(150)
   end
+
+  it 'calculates proper position using aim' do
+    commands = [
+      'forward 5',
+      'down 5',
+      'forward 8',
+      'up 3',
+      'down 8',
+      'forward 2'
+    ]
+
+    expect(day2p2(commands)).to eq(900)
+  end
 end
