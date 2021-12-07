2121c4398ae882c5b32eb39aab4fa0f625d51147
Jakub Rodzik <jakub.r.rodzik@gmail.com>
2021-12-07 09:36:34 +0100

    day7p2

--- day7/day7.rb
+++ day7/day7.rb
@@ -1,12 +1,9 @@
-require 'pry'
-
 def day7p1(positions)
-  positions.sort!
   prev_sum = Float::INFINITY
   i = 0
 
   while i < positions.length
-    sum = positions.reduce(0) { |acc, e| acc + (e - positions[i]).abs }
+    sum = positions.reduce(0) { |acc, e| acc + (e - i).abs }
     break if sum > prev_sum
 
     prev_sum = sum
@@ -18,3 +15,24 @@ end
 
 input = File.read('day7/input.txt').split(',').map(&:to_i)
 p day7p1(input)
+
+def day7p2(positions)
+  prev_sum = Float::INFINITY
+  i = 0
+
+  while i < positions.length
+    sum = positions.reduce(0) { |acc, e| acc + distance_sum((e - i).abs) }
+    break if sum > prev_sum
+
+    prev_sum = sum
+    i += 1
+  end
+
+  prev_sum.to_i
+end
+
+def distance_sum(a)
+  (a / 2.0) * (a + 1)
+end
+
+p day7p2(input)
--- day7/day7_spec.rb
+++ day7/day7_spec.rb
@@ -4,8 +4,14 @@ require_relative 'day7'
 
 RSpec.describe 'Day 7' do
   it 'finds fuel expense for the best position' do
-    crab_positions = [16,1,2,0,4,2,7,1,2,14]
+    crab_positions = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
 
     expect(day7p1(crab_positions)).to eq(37)
+  end
+
+  it 'finds fuel expense for the best position' do
+    crab_positions = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
+
+    expect(day7p2(crab_positions)).to eq(168)
   end
 end
