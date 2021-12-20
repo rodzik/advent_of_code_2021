38e5e467a14d5cfe3494377a8b0f5d1a5c5d8c1d
Jakub Rodzik <jakub.r.rodzik@gmail.com>
2021-12-20 14:55:27 +0100

    day16p2

--- day16/day16.rb
+++ day16/day16.rb
@@ -1,35 +1,59 @@
-require 'pry'
-
-def sum(hex)
+def day16(hex)
   binary = hex.chars.map{sprintf('%04b', _1.to_i(16))}.join("")
-  sum_version(binary)
+  sum(binary)
 end
 
+def sum(binary, mode = '100', breaker = nil)
+  version_sum = 0
+  values = []
+  i = 0
 
-def sum_version(binary)
-  sum = 0
-  while binary.size > 0
+  while binary.size > 0 && breaker != i
     version = binary.slice!(0, 3).to_i(2)
     type = binary.slice!(0, 3)
-    sum += version
+    version_sum += version
+    i += 1
 
     if type == '100'
-      true while binary.slice!(0, 5)[0] == '1'
-    else
-      o_t = binary.slice!(0, 1)
-
-      if o_t == '0'
-        number = binary.slice!(0, 15).to_i(2)
-        sum += sum_version(binary.slice!(0, number))
-      else
-        binary.slice!(0, 11)
+      literal = ''
+      while true
+        group = binary.slice!(0, 5)
+        literal << group[1..]
+        break if group[0] == '0'
       end
+
+      values << literal.to_i(2)
+    else
+      if binary.slice!(0, 1) == '0'
+        subpackets_length = binary.slice!(0, 15).to_i(2)
+        result = sum(binary.slice!(0, subpackets_length), type)
+      else
+        subpackets_count = binary.slice!(0, 11).to_i(2)
+        result = sum(binary, type, subpackets_count)
+      end
+
+      version_sum += result[:versions]
+      values << result[:values]
     end
   end
 
-  sum
+  { versions: version_sum, values: reduce(mode, values) }
+end
+
+def reduce(mode, values)
+  values.flatten!
+  case mode.to_i(2)
+  when 0 then values.reduce(:+)
+  when 1 then values.reduce(:*)
+  when 2 then values.min
+  when 3 then values.max
+  when 4 then values[0]
+  when 5 then values[0] > values[1] ? 1 : 0
+  when 6 then values[0] < values[1] ? 1 : 0
+  when 7 then values[0] == values[1] ? 1 : 0
+  end
 end
 
 input = File.read('day16/input.txt')
-p sum(input)
+p day16(input)
 
--- day16/day16_spec.rb
+++ day16/day16_spec.rb
@@ -1,40 +1,77 @@
 require_relative 'day16'
 
 RSpec.describe 'Day 16' do
-  it 'sums versions' do
-    input = 'D2FE28'
-    expect(sum(input)).to eq(6)
+  describe '#sum_versions' do
+    it 'sums versions' do
+      input = 'D2FE28'
+      expect(day16(input)[:versions]).to eq(6)
+    end
+
+    it 'sums versions' do
+      input = '38006F45291200'
+      expect(day16(input)[:versions]).to eq(9)
+    end
+
+    it 'sums versions' do
+      input = 'EE00D40C823060'
+      expect(day16(input)[:versions]).to eq(14)
+    end
+
+    it 'sums versions' do
+      input = '8A004A801A8002F478'
+      expect(day16(input)[:versions]).to eq(16)
+    end
+
+    it 'sums versions' do
+      input = '620080001611562C8802118E34'
+      expect(day16(input)[:versions]).to eq(12)
+    end
+
+    it 'sums versions' do
+      input = 'C0015000016115A2E0802F182340'
+      expect(day16(input)[:versions]).to eq(23)
+    end
+
+    it 'sums versions' do
+      input = 'A0016C880162017C3686B18A3D4780'
+      expect(day16(input)[:versions]).to eq(31)
+    end
   end
 
-  it 'sums versions' do
-    input = '38006F45291200'
-    expect(sum(input)).to eq(9)
-  end
+  describe '#sum_values' do
+    it 'calculates values' do
+      input = 'C200B40A82'
+      expect(day16(input)[:values]).to eq(3)
+    end
 
-  it 'sums versions' do
-    input = 'EE00D40C823060'
-    expect(sum(input)).to eq(14)
-  end
+    it 'calculates values' do
+      input = '04005AC33890'
+      expect(day16(input)[:values]).to eq(54)
+    end
 
-  it 'sums versions' do
-    input = '8A004A801A8002F478'
-    expect(sum(input)).to eq(16)
-  end
+    it 'calculates values' do
+      input = '880086C3E88112'
+      expect(day16(input)[:values]).to eq(7)
+    end
 
-  it 'sums versions' do
-    input = '620080001611562C8802118E34'
-    expect(sum(input)).to eq(12)
-  end
+    it 'calculates values' do
+      input = 'CE00C43D881120'
+      expect(day16(input)[:values]).to eq(9)
+    end
 
-  it 'sums versions' do
-    input = 'C0015000016115A2E0802F182340'
-    expect(sum(input)).to eq(23)
-  end
+    it 'calculates values' do
+      input = 'D8005AC2A8F0'
+      expect(day16(input)[:values]).to eq(1)
+    end
 
-  it 'sums versions' do
-    input = 'A0016C880162017C3686B18A3D4780'
-    expect(sum(input)).to eq(31)
+    it 'calculates values' do
+      input = 'F600BC2D8F'
+      expect(day16(input)[:values]).to eq(0)
+    end
+
+    it 'calculates values' do
+      input = '9C0141080250320F1802104A08'
+      expect(day16(input)[:values]).to eq(1)
+    end
   end
 end
-
-
